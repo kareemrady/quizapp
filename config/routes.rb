2a3e74a4a07f65e3cf8c7cba0a06c719105e21a9
Kareem <kareem_rady@yahoo.com>
2015-11-29 17:14:45 +0000

    Added a post model and posts links on main nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Quizapp::Application.routes.draw do
   root 'quizapp#index'
   get 'about' => 'quizapp#about'
+  resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
